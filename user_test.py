06e36350d04df15c49a1aff55d672cc2fbc3780a
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-29 09:05:48 +0300

    Create a method to test if object is initialized correctly

--- user_test.py
+++ user_test.py
@@ -7,4 +7,14 @@ class TestUser(unittest.TestCase):
         """
         self.new_user = User("Fitstar","Gillie2021")
 
+    def test_init(self):
+        """
+        test case to check if the object in initialized correctly
+        """
+        self.assertEqual(self.new_user.username,"Fitstar")
+        self.assertEqual(self.new_user.password,"Gillie2021")
+
+if __name__ == '__main__':
+    unittest.main()
+
   
