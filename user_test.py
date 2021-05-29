ba505dd0e54e3f4e9d7d79f8308a228ba5168e81
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-29 09:17:29 +0300

    Create a method to test if the user is saved

--- user_test.py
+++ user_test.py
@@ -13,6 +13,14 @@ class TestUser(unittest.TestCase):
         """
         self.assertEqual(self.new_user.username,"Fitstar")
         self.assertEqual(self.new_user.password,"Gillie2021")
+    
+    def test_save_user(self):
+        '''
+        test case to check if the new instance of the user object has been created
+        '''
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == '__main__':
     unittest.main()
