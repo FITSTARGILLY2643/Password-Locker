035eafae4726a74f4e1c90b7022def5e2b36cb8b
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-29 08:57:55 +0300

    Create the TestUser class

--- user_test.py
+++ user_test.py
@@ -1,3 +1,10 @@
 import unittest
 from user import User
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """
+        A method that runs before the test starts
+        """
+        self.new_user = User("Fitstar","Gillie2021")
 
+  
