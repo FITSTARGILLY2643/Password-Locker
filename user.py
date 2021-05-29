23783816a5458a6d4e0fbe1b8f1592c992814f5b
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-29 08:45:05 +0300

    Create class User

--- user.py
+++ user.py
@@ -1,2 +1,10 @@
 import random
 import string
+class User:
+    """
+    Class that generates instance of a user,list of users and their passwords
+    """
+    user_list=[]
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
